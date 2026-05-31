b170787dfb100138b9ec510f577a1c063a83edf8
Double_Circle <qqf1430550377@outlook.com>
2026-05-31 20:07:50 +0800

    规范化输入

--- vue-TelemetryInstruments/vite.config.js
+++ vue-TelemetryInstruments/vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"), // 关键：将 @ 映射到 src 目录
+        },
+    },
     build: {
         // 输出到 Jekyll 的 assets/vue-telemetry 下
-        outDir: "../assets/vue-telemetry",
+        outDir: "../assets/vue-app",
         emptyOutDir: true,
         rollupOptions: {
             output: {

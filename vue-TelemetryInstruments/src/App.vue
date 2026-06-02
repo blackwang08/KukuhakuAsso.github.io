7e333160d9f80433075229fb134d7b3e48f4f3e0
Ku²haku Assoiciotion <3625413735@qq.com>
2026-06-02 17:35:54 +0800

    Update App.vue

--- vue-TelemetryInstruments/src/App.vue
+++ vue-TelemetryInstruments/src/App.vue
@@ -4,6 +4,8 @@ import AudioPlayer from './components/audioPlayer.vue';
 </script>
 
 <template>
-  <PuzzlePage title="谜题名称" content="<p>谜题描述 HTML</p>" level="1" />
-  <AudioPlayer />
+  <div id="app">
+    <AudioPlayer />
+    <router-view />
+  </div>
 </template>

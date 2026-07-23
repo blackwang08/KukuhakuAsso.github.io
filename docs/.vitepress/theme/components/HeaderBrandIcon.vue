e1cf841450cc440936b83ab29b1fc3072fdb03fb
blackwang08 <1580716052@qq.com>
2026-07-23 09:19:55 +0800

    代码格式修正

--- docs/.vitepress/theme/components/HeaderBrandIcon.vue
+++ docs/.vitepress/theme/components/HeaderBrandIcon.vue
@@ -1,16 +1,16 @@
 <template>
-    <span class="header-brand-icon" aria-hidden="true"></span>
+  <span class="header-brand-icon" aria-hidden="true"></span>
 </template>
 
 <style scoped>
 .header-brand-icon {
-    width: 24px;
-    height: 24px;
-    background: url("/favicon.svg") center / contain no-repeat;
-    flex: 0 0 auto;
+  width: 24px;
+  height: 24px;
+  background: url("/favicon.svg") center / contain no-repeat;
+  flex: 0 0 auto;
 }
 
 :global(html.dark) .header-brand-icon {
-    background-image: url("/favicon_white.svg");
+  background-image: url("/favicon_white.svg");
 }
 </style>
